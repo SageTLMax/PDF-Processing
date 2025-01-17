b6324f4c06b52ba43f58fc4485fec9cceee29a0a
SageTLMax <samaxw@mit.edu>
2025-01-17 13:26:57 -1000

    Initial Commit
    Adding current existing code to repository.
    - Mass merge images to PDF by folder
    - Functions to apply different compression methods (pdfsizeopt; GhostScript)

---
+++ compress_pdf.py
@@ -0,0 +1,42 @@
+import subprocess
+import os
+import ghostscript
+
+PATH_TO_MERGED_PDFS = "./ExportedPDFs/"
+
+def compress_with_pdfsizeopt():
+    pdfs_to_compress = ["." + PATH_TO_MERGED_PDFS + f for f in os.listdir(PATH_TO_MERGED_PDFS)]
+
+    os.chdir("./pdfsizeopt/")
+    for pdf_name in pdfs_to_compress:
+        # Compress the PDF and save in the ExportedPDFs folder.
+        # Add "--use-pngout=no" after "pdfsizeopt" if runtime is slow.
+        subprocess.run(["cmd.exe", "/c", "pdfsizeopt", pdf_name, f"./{pdf_name}"])
+
+def compress_with_ghostscript():
+    # Path names for PDFs to compress.
+    pdfs_to_compress = [PATH_TO_MERGED_PDFS + f for f in os.listdir(PATH_TO_MERGED_PDFS)]
+
+    def compress_single_pdf(index):
+        # Settings for Ghostscript compression.
+        args = [
+            "-q",
+            "-dNOPAUSE",
+            "-dBATCH",
+            "-dSAFER",
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dPDFSETTINGS=/screen",
+            "-dColorImageDownsampleType=/Bicubic",
+            "-dColorImageResolution=144", 
+            f"-sOutputFile={PATH_TO_MERGED_PDFS}output{index}.pdf", # output file
+            pdfs_to_compress[index],                                            # input file
+        ]
+
+        # Compress with ghostscript.
+        ghostscript.Ghostscript(*args)
+    
+    for pdf_num in range(len(pdfs_to_compress)):
+        compress_single_pdf(pdf_num)
+
+compress_with_ghostscript()
---
+++ merge_to_pdf.py
@@ -0,0 +1,36 @@
+from PIL import Image 
+import os
+import regex
+
+PATH_TO_FOLDERS = "./FoldersForMerging/"
+
+# Regex used to check if file is an image.
+IS_IMAGE_REGEX = "^\.pdf|\.jpg|\.jpeg|\.tif|\.png|\.pdf$"
+
+# Get the names of all folders which we want to create merged PDFs for.
+folder_paths = []
+folder_names = []
+for folder_name in os.listdir(PATH_TO_FOLDERS):
+    folder_paths.append("./FoldersForMerging/" + folder_name + "/" )
+    folder_names.append(folder_name)
+
+# Make a merged PDF of all images for a given folder.
+def merge_to_pdf(filepath, filename):
+    # Returns a list of all the images in a given folder.
+    images = [
+        Image.open(filepath + f) for f in os.listdir(filepath) 
+            # Only use the image if it has an image-type file extension (.jpg, .tif, etc.)
+            if regex.findall(IS_IMAGE_REGEX, f, regex.IGNORECASE, overlapped=True)
+    ]
+
+    # Where to save the created PDF.
+    pdf_path = "./ExportedPDFs/" + filename + ".pdf"
+        
+    # Merge images and save the PDF.
+    images[0].save(
+        pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
+    )
+
+# Make a merged PDF for each desired folder.
+for folder_ind in range(len(folder_paths)):
+    merge_to_pdf(folder_paths[folder_ind], folder_names[folder_ind])
